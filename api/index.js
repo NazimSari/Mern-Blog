80dbba4208b1000deae80ee4664f93ba286bb9f6
Nazim <yasarnazimsari@gmail.com>
2024-01-24 17:03:32 +0300

    signup function

--- api/index.js
+++ api/index.js
@@ -2,17 +2,23 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
+const app = express();
+//middleware
+app.use(express.json());
+
+//database
 mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-const app = express();
+//routes
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
-
-app.use("/api/user", userRoutes);
