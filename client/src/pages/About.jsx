9f5b132c2532d71f7cf367279c972bd2107138b1
Nazim <yasarnazimsari@gmail.com>
2024-02-08 22:14:26 +0300

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 function About() {
-  return <div>about</div>;
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Mern Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              eos rerum veritatis. Vel, corporis dignissimos id quia excepturi
+              repellendus vero? Exercitationem illo quo culpa nam minus rerum
+              inventore cum molestias!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              eos rerum veritatis. Vel, corporis dignissimos id quia excepturi
+              repellendus vero? Exercitationem illo quo culpa nam minus rerum
+              inventore cum molestias!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              eos rerum veritatis. Vel, corporis dignissimos id quia excepturi
+              repellendus vero? Exercitationem illo quo culpa nam minus rerum
+              inventore cum molestias!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About;
